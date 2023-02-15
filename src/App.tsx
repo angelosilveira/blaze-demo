daad546e8259c129f1af95b849cbfdc07000b5ce
Ângelo Patricio Soares <angelo.silveira09@gmail.com>
2023-02-15 13:46:31 -0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { ThemeProvider } from 'styled-components';
-import { ToastContainer } from 'react-toastify';
+
 import { RoutesComponent } from './routes';
 import GlobalStyles from './styles/global';
 import theme from './styles/theme';
@@ -8,17 +8,6 @@ import 'react-toastify/dist/ReactToastify.css';
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <ToastContainer
-        position="top-right"
-        theme="colored"
-        autoClose={10000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-      />
       <GlobalStyles />
       <RoutesComponent />
     </ThemeProvider>
